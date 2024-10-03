92ea869746754bb23949f769a49d96b34d0dddf1
korimart <korimart2019@gmail.com>
2024-10-03 16:31:30 +0900

    10828

---
+++ 10828/Source.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+#include <string>
+#include <sstream>
+#include <stack>
+
+int main()
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    int InstructionCount;
+    std::cin >> InstructionCount;
+
+    std::string Nothing;
+    std::getline(std::cin, Nothing);
+
+    std::stack<int> Stack;
+    for (int i = 0; i < InstructionCount; i++)
+    {
+        std::string Line;
+        std::getline(std::cin, Line);
+        
+        std::istringstream LineStream{Line};
+        std::string Instruction;
+        LineStream >> Instruction;
+
+        if (Instruction == "push")
+        {
+            int Parameter;
+            LineStream >> Parameter;
+            Stack.push(Parameter);
+        }
+        else if (Instruction == "pop")
+        {
+            std::cout << (Stack.empty() ? -1 : Stack.top()) << "\n";
+            Stack.empty() ? (void)0 : Stack.pop();
+        }
+        else if (Instruction == "size")
+        {
+            std::cout << Stack.size() << "\n";
+        }
+        else if (Instruction == "empty")
+        {
+            std::cout << (Stack.empty() ? 1 : 0) << "\n";
+        }
+        else if (Instruction == "top")
+        {
+            std::cout << (Stack.empty() ? -1 : Stack.top()) << "\n";
+        }
+    }
+}
