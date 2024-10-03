d83df2be8dd16ed4cca56e94ff378bfce797f773
korimart <korimart2019@gmail.com>
2024-10-03 17:10:37 +0900

    10799

---
+++ 10799/Source.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <string>
+
+int main()
+{
+    std::string Parentheses;
+    std::cin >> Parentheses;
+
+    int PieceCount = 0;
+    int BarCount = 0;
+    for (int i = 0; i < Parentheses.size(); i++)
+    {
+        if (Parentheses[i] == '(')
+        {
+            PieceCount++;
+            BarCount++;
+            continue;
+        }
+
+        const bool bLaser = i > 0 && Parentheses[i - 1] == '(';
+        
+        if (bLaser)
+        {
+            // ()는 막대기가 아니라 레이저임 그래서 아까 1 올린 거 다시 내림
+            PieceCount--;
+            BarCount--;
+
+            // 레이저로 잘랐으므로 조각 카운트 막대기의 개수만큼 UP
+            PieceCount += BarCount;
+        }
+        else
+        {
+            BarCount--;
+        }
+    }
+
+    std::cout << PieceCount << std::endl;
+}
