430308290decfacfe1c514ce5590229394ff492a
korimart <korimart2019@gmail.com>
2024-10-04 18:23:49 +0900

    2075

---
+++ 2075/Source.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <queue>
+
+int main()
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    int Dimension;
+    std::cin >> Dimension;
+
+    std::priority_queue<int, std::vector<int>, std::greater<>> PriorityQueue;
+    for (int i = 0; i < Dimension * Dimension; i++)
+    {
+        int Entry;
+        std::cin >> Entry;
+        PriorityQueue.push(Entry);
+
+        while (PriorityQueue.size() > Dimension)
+        {
+            PriorityQueue.pop();
+        }
+    }
+
+    std::cout << PriorityQueue.top() << std::endl;
+}
