c2e62a76df8c2049e743a4c4ee83e144254c579e
korimart <korimart2019@gmail.com>
2024-10-08 16:08:32 +0900

    11726

---
+++ 11726/Source.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <vector>
+
+int main()
+{
+    int N;
+    std::cin >> N;
+
+    std::vector<int> MemoizationCache(N);
+    MemoizationCache[0] = 1;
+    MemoizationCache[1] = 2;
+    for (int i = 2; i < N; i++)
+    {
+        MemoizationCache[i] = (MemoizationCache[i - 1] + MemoizationCache[i - 2]) % 10'007;
+    }
+    
+    std::cout << MemoizationCache.back();
+}
