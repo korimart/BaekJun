e968a6bd28f422995d64db11a3cab8f77f19b693
korimart <korimart2019@gmail.com>
2024-10-10 13:54:54 +0900

    1912

---
+++ 1912/Source.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main()
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    
+    int SequenceSize;
+    std::cin >> SequenceSize;
+
+    std::vector<int> Sequence;
+    Sequence.reserve(SequenceSize);
+    for (int i = 0; i < SequenceSize; ++i)
+    {
+        int Element;
+        std::cin >> Element;
+        Sequence.push_back(Element);
+    }
+
+    int PrevMax = Sequence[0];
+    int AllTimeMax = PrevMax;
+    for (int i = 1; i < SequenceSize; ++i)
+    {
+        const int This = Sequence[i];
+        const int PrevMaxPlusThis = PrevMax + This;
+        const int ThisMax = This < PrevMaxPlusThis ? PrevMaxPlusThis : This;
+        AllTimeMax = std::max(AllTimeMax, ThisMax);
+        PrevMax = ThisMax;
+    }
+    
+    std::cout << AllTimeMax;
+}
