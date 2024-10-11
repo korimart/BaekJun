07734b3ec0a9b31fc1dbe0972cd7d76abc485ae2
korimart <korimart2019@gmail.com>
2024-10-11 15:50:35 +0900

    10844

---
+++ 10844/Source.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <array>
+
+int main()
+{
+    int DigitCount;
+    std::cin >> DigitCount;
+
+    std::array<std::array<int, 100>, 10> NumbersEndingWithCount{};
+    NumbersEndingWithCount[1][0] = 1;
+    NumbersEndingWithCount[2][0] = 1;
+    NumbersEndingWithCount[3][0] = 1;
+    NumbersEndingWithCount[4][0] = 1;
+    NumbersEndingWithCount[5][0] = 1;
+    NumbersEndingWithCount[6][0] = 1;
+    NumbersEndingWithCount[7][0] = 1;
+    NumbersEndingWithCount[8][0] = 1;
+    NumbersEndingWithCount[9][0] = 1;
+
+    for (int DigitIndex = 1; DigitIndex < DigitCount; ++DigitIndex)
+    {
+        NumbersEndingWithCount[1][DigitIndex] += NumbersEndingWithCount[0][DigitIndex - 1];
+        NumbersEndingWithCount[8][DigitIndex] += NumbersEndingWithCount[9][DigitIndex - 1];
+
+        for (int Number = 1; Number < 9; ++Number)
+        {
+            NumbersEndingWithCount[Number - 1][DigitIndex] += NumbersEndingWithCount[Number][DigitIndex - 1];
+            NumbersEndingWithCount[Number + 1][DigitIndex] += NumbersEndingWithCount[Number][DigitIndex - 1];
+        }
+
+        for (auto& Each : NumbersEndingWithCount)
+        {
+            Each[DigitIndex] %= 1'000'000'000;
+        }
+    }
+
+    int Sum = 0;
+    for (int i = 0; i < 10; ++i)
+    {
+        Sum += NumbersEndingWithCount[i][DigitCount - 1];
+        Sum %= 1'000'000'000;
+    }
+    std::cout << Sum << std::endl;
+}
