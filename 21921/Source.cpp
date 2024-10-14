3cf1d27583a18d814c07a3d1fbcf34229f6a9ae2
korimart <korimart2019@gmail.com>
2024-10-14 14:12:13 +0900

    21921

---
+++ 21921/Source.cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream>
+#include <vector>
+#include <numeric>
+
+int main()
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    int DayCount, WindowSize;
+    std::cin >> DayCount >> WindowSize;
+
+    std::vector<int> VisitorCounts;
+    VisitorCounts.reserve(DayCount);
+    for (int i = 0; i < DayCount; ++i)
+    {
+        int VisitorCount;
+        std::cin >> VisitorCount;
+        VisitorCounts.push_back(VisitorCount);
+    }
+
+    int LargestSum = 0;
+    int LargestSumCount = 0;
+
+    auto WindowFirst = VisitorCounts.begin();
+    auto WindowLast = VisitorCounts.begin() + WindowSize - 1;
+    
+    int WindowSum = std::accumulate(WindowFirst, WindowLast + 1, 0);
+    WindowSum -= *WindowLast;
+    
+    while (WindowLast != VisitorCounts.end())
+    {
+        WindowSum += *WindowLast;
+
+        if (WindowSum > LargestSum)
+        {
+            LargestSum = WindowSum;
+            LargestSumCount = 1;
+        }
+        else if (WindowSum == LargestSum)
+        {
+            LargestSumCount++;
+        }
+        
+        WindowSum -= *WindowFirst;
+        ++WindowFirst;
+        ++WindowLast;
+    }
+
+    if (LargestSum == 0)
+    {
+        std::cout << "SAD";
+    }
+    else
+    {
+        std::cout << LargestSum << "\n" << LargestSumCount;
+    }
+}
