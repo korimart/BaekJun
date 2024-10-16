bd57791a0c9bf9800b5e2079457c5a64da700317
korimart <korimart2019@gmail.com>
2024-10-16 21:13:07 +0900

    1717

---
+++ 1717/Source.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <vector>
+#include <numeric>
+
+int main()
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    int ElementCount, InstructionCount;
+    std::cin >> ElementCount >> InstructionCount;
+    ElementCount++;
+
+    std::vector<int> Roots(ElementCount);
+    std::iota(Roots.begin(), Roots.end(), 0);
+
+    const auto FindTopMost = [&](const auto& Self, int Element)
+    {
+        if (Roots[Element] == Element)
+        {
+            return Element;
+        }
+
+        return Roots[Element] = Self(Self, Roots[Element]);
+    };
+
+    for (int i = 0; i < InstructionCount; ++i)
+    {
+        int Instruction, Operand0, Operand1;
+        std::cin >> Instruction >> Operand0 >> Operand1;
+
+        const int Root0 = FindTopMost(FindTopMost, Operand0);
+        const int Root1 = FindTopMost(FindTopMost, Operand1);
+        
+        if (Instruction == 0)
+        {
+            if (Root0 != Root1)
+            {
+                Roots[Root1] = Root0;
+            }
+        }
+        else
+        {
+            std::cout << (Root0 == Root1 ? "YES" : "NO") << '\n';
+        }
+    }
+}
