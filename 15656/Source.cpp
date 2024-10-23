a844d628b05a476cec19781c395c453d7e6a2cae
korimart <korimart2019@gmail.com>
2024-10-23 16:24:58 +0900

    15656

---
+++ 15656/Source.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <algorithm>
+
+int main()
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    
+    int CandidateCount, SequenceSize;
+    std::cin >> CandidateCount >> SequenceSize;
+
+    int Candidates[10];
+    for (int i = 0; i < CandidateCount; ++i)
+    {
+        std::cin >> Candidates[i];
+    }
+    std::sort(Candidates, Candidates + CandidateCount);
+
+    int Sequence[10];
+    const auto ChooseOneForIndex = [&](const auto& Self, int Index, const auto& OnSequenceComplete)
+    {
+        if (Index >= SequenceSize)
+        {
+            OnSequenceComplete();
+            return;
+        }
+
+        std::for_each_n(Candidates, CandidateCount, [&](int Each)
+        {
+            Sequence[Index] = Each;
+            Self(Self, Index + 1, OnSequenceComplete);
+        });
+    };
+
+    ChooseOneForIndex(ChooseOneForIndex, 0, [&]()
+    {
+        std::for_each_n(Sequence, SequenceSize, [&](int Each) { std::cout << Each << ' '; });
+        std::cout << '\n';
+    });
+}
